1fb028512610853f754264997e43cd9e37fb825f
magsinomichael <22-16399@g.batstate-u.edu.ph>
2025-06-28 08:35:46 +0800

    l&a

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WhileLoopExample
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int sum;
+
+            Console.WriteLine("Enter a number to add. Note: if you type zero(0) number input will stop");
+
+            while (true)
+            {
+                Console.Write("Enter a number: ");
+                number = Convert.ToInt32(Console.ReadLine());
+
+                if (number == 0)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
